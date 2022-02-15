77233cf0d0508ced221caa993972aa0e94eed598
kim <saugkim@users.noreply.github.com>
2022-02-15 14:29:46 +0200

    part2 courses subtask1 ready

---
+++ part2/courses/src/components/Contents.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import Part from './Part'
+
+const Contents = ( {parts} ) => {
+  return (
+    <ul>    
+      {parts.map(part=> <Part key={part.id} name={part.name} exercises={part.exercises} />)}
+    </ul>
+  )
+}
+  
+export default Contents
+  
---
+++ part2/courses/src/components/Course.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+import Header from './Header'
+import Contents from './Contents'
+
+const Course = ( {course} ) => {
+    return(
+        <div>
+            <Header name={course.name}></Header>
+            <Contents parts={course.parts}></Contents> />
+        </div>
+    )
+}
+
+export default Course
